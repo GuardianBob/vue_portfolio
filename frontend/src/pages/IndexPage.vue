6bbd688d9784eb4d5f8b382c13698d26dbf4567f
Jesse Meyer <78508863+GuardianBob@users.noreply.github.com>
2023-03-04 18:59:10 -0800

    Added buttons with links

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,11 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="row flex flex-center q-mt-lg">
+  <q-page class="items-center">
+    <div class="row flex flex-center q-mt-xl q-mb-sm">
         <img alt="JBear Creations" src="~assets/JBear_Logo_front_Orange.png" style="width: 25%;">
         <img alt="JBear Creations" src="~assets/JBear_Logo_Saw_Orange.png" style="width: 25%;" class="big-saw" @click="spin_saw">
         <img alt="JBear Creations" src="~assets/JBear_Logo_back_Orange.png" style="width: 25%;">
     </div>
-    <div>
+    <div class="row justify-center">
+      <div class="col-10 text-center">
+        <h6 class="q-mt-none q-mb-md" style="font-family:'Splash';">Don't wait for that day to come, make it happen!</h6>
+      </div>
+    </div>
+    <div class="row justify-evenly">
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-10 text-center">
+        <q-btn class="q-px-auto q-my-sm full-width" outline color="primary" href="https://guardianbob.github.io" target="_blank">
+          About
+          <q-tooltip class="bg-accent">Yo!</q-tooltip>
+        </q-btn>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-10 text-center">
+      <q-btn class="q-px-auto q-my-sm full-width" outline color="primary" to="/workshop">
+        Workshop
+        <q-tooltip class="bg-accent">Get ready for awesome!</q-tooltip>
+      </q-btn>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-10 text-center">
+      <q-btn class="q-px-auto q-my-sm full-width" outline color="primary" to="/coding">
+        Coding Den
+        <q-tooltip class="bg-accent">I'm just getting started...</q-tooltip>
+      </q-btn>
+      </div>
     </div>
   </q-page>
 </template>
